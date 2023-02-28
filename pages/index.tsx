5f911b7602c72d1c17af5ab446f784b70c3a58bb
Ryan Carter <whiteapple8222@gmail.com>
2023-02-28 09:24:03 +0000

    update for prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,6 @@ const HomePage: NextPage = () => {
     <Default pageName="Home">
       {/* <Home /> */}
       <NFTBalances />
-
     </Default>
   );
 };
--- src/components/templates/balances/NFT/NFTBalances.tsx
+++ src/components/templates/balances/NFT/NFTBalances.tsx
@@ -12,7 +12,7 @@ const NFTBalances = () => {
     address: data?.user?.address,
     chain: chain?.id,
   });
-  const filteraddress = "0x8b633f357e590f726642ec8eccab0c1a56dbe4be"
+  const filteraddress = '0x8b633f357e590f726642ec8eccab0c1a56dbe4be';
 
   useEffect(() => console.log('nfts: ', nfts), [nfts]);
 
@@ -23,11 +23,7 @@ const NFTBalances = () => {
       </Heading>
       {nfts?.length ? (
         <Grid templateColumns="repeat(auto-fit, minmax(280px, 1fr))" gap={6}>
-          {nfts.map((nft, key) => (
-              nft.tokenAddress.lowercase == filteraddress && (
-                <NFTCard nft={nft} key={key} />
-              )
-          ))}
+          {nfts.map((nft, key) => nft.tokenAddress.lowercase == filteraddress && <NFTCard nft={nft} key={key} />)}
         </Grid>
       ) : (
         <Box>Looks Like you do not have any NFTs</Box>
